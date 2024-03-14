53f01c33dd2d02356df869c5dd9757809b6fca8c
Karolina Slemp-Rejowska <slempkarolina@gmail.com>
2024-03-14 15:13:06 +0100

    finished top section

--- app/src/main/java/com/karolina/jetpack/loginscreencompose/LoginScreen.kt
+++ app/src/main/java/com/karolina/jetpack/loginscreencompose/LoginScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -18,6 +20,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -35,43 +38,57 @@ import com.karolina.jetpack.loginscreencompose.ui.theme.BlueGray
 fun LoginScreen() {
     Surface {
         Column(modifier = Modifier.fillMaxSize()) {
-            val uiColor: Color = if (isSystemInDarkTheme()) Color.White else Black
-            Box(contentAlignment = Alignment.TopCenter) {
+            TopSection()
+        }
+    }
+}
 
-                Image(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillMaxHeight(fraction = 0.46f),
-                    painter = painterResource(id = R.drawable.shape),
-                    contentDescription = null,
-                    contentScale = ContentScale.FillBounds,
-                    colorFilter = if (isSystemInDarkTheme()) ColorFilter.tint(BlueGray) else null
+@Composable
+private fun TopSection() {
+    val uiColor: Color = if (isSystemInDarkTheme()) Color.White else Black
+    Box(contentAlignment = Alignment.TopCenter) {
+
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(fraction = 0.46f),
+            painter = painterResource(id = R.drawable.shape),
+            contentDescription = null,
+            contentScale = ContentScale.FillBounds,
+            colorFilter = if (isSystemInDarkTheme()) ColorFilter.tint(BlueGray) else null
+        )
+
+        Row(modifier = Modifier.padding(top = 80.dp)) {
+            Icon(
+                modifier = Modifier.size(42.dp),
+                painter = painterResource(id = R.drawable.logo),
+                contentDescription = stringResource(id = R.string.app_logo),
+                tint = uiColor
+            )
+
+            Spacer(modifier = Modifier.width(15.dp))
+
+            Column {
+                Text(
+                    text = stringResource(id = R.string.tolet),
+                    style = MaterialTheme.typography.headlineMedium,
+                    color = uiColor
                 )
-
-                Row(modifier = Modifier.padding(top = 80.dp)) {
-                    Icon(
-                        modifier = Modifier.size(42.dp),
-                        painter = painterResource(id = R.drawable.logo),
-                        contentDescription = stringResource(id = R.string.app_logo),
-                        tint = uiColor
-                    )
-
-                    Spacer(modifier = Modifier.width(15.dp))
-
-                    Column {
-                        Text(
-                            text = stringResource(id = R.string.the_tolet),
-                            style = MaterialTheme.typography.headlineMedium,
-                            color = uiColor
-                        )
-                        Text(
-                            text = stringResource(id = R.string.find_house),
-                            style = MaterialTheme.typography.titleMedium,
-                            color = uiColor
-                        )
-                    }
-                }
+                Text(
+                    text = stringResource(id = R.string.house),
+                    style = MaterialTheme.typography.titleMedium,
+                    color = uiColor
+                )
             }
         }
+
+        Text(
+            modifier = Modifier
+                .padding(bottom = 10.dp)
+                .align(alignment = Alignment.BottomCenter),
+            text = stringResource(id = R.string.login),
+            style = MaterialTheme.typography.headlineLarge,
+            color = uiColor
+        )
     }
 }
